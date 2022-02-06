80eb7f75aec00781609ea1df404acf71fb22363e
Diego Inostroza <diego.inoca@gmail.com>
2022-02-06 10:31:40 -0300

    capture branch

--- bin/main/ejecucion.groovy
+++ bin/main/ejecucion.groovy
@@ -1,12 +1,34 @@
 def call(){
     pipeline {
         agent any
+        environment {
+            NEXUS_USER         = credentials('NEXUS-USER')
+            NEXUS_PASSWORD     = credentials('NEXUS-PASS')
+        }
+
         stages {
             stage("Pipeline"){
-                figlet 'Iniciando ejecución Pipeline'
+                steps {
+                    script{
+                        //def ci_or_cd = verifyBranchName()
+                        figlet env.GIT_BRANCH
+                    }
+                }
+                post{
+                    figlet 'Finalizado Pipeline'
+                }
             }
         }
     }
+}
+
+
+def verifyBranchName(){
+	if(env.GIT_BRANCH.contains('feature-') || env.GIT_BRANCH.contains('develop')) {
+		return 'CI'
+	} else {
+		return 'CD'
+	}
 }
 
 return this;
