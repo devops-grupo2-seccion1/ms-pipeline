be733b96c7beede818a5fd38755d7375209bc8e2
Diego Inostroza <diego.inoca@gmail.com>
2022-02-06 14:20:01 -0300

    base other project

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -1,15 +1,29 @@
 def call(){
+
     pipeline {
         agent any
-        environment {
-            NEXUS_USER         = credentials('NEXUS-USER')
-            NEXUS_PASSWORD     = credentials('NEXUS-PASS')
+
+        parameters { 
+            choice(name: 'buildtool', choices: ['maven','gradle'], description: 'Elección de herramienta de construcción para aplicación covid')
+            string(name: 'stages', defaultValue: '' , description: 'Escribir stages a ejecutar en formato: stage1;stage2;stage3. Si stage es vacío, se ejecutarán todos los stages.')
         }
+
         stages {
-            stage("Pipeline"){
+            stage('Pipeline') {
                 steps {
                     script{
-                         maven.call()
+
+                        sh 'env'
+
+                        figlet params.buildtool
+                        def archivo = (params.buildtool == 'gradle') ? 'build.gradle' : 'pom.xml'
+
+                        if (fileExists(archivo)){
+                            "${params.buildtool}" "${params.stages}"
+                        } else {
+                            error "archivo ${archivo} no existe. No se puede construir pipeline basado en ${params.buildtool}"
+                        }
+
                     }
                 }
             }
--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,11 +1,88 @@
 import pipeline.*
 
-def call(){
+def call(String chosenStages){
 
-    def utils  = new test.ValidateUtility()
+    def utils  = new test.UtilMethods()
+
+    def pipelineStages = (utils.isCIorCD().contains('ci')) ? ['compile','test','jar','sonar','runJar','rest','nexusCI','crearRelease'] : ['downloadNexus','runDownloadedJar','rest','nexusCD'] 
+
+    def stages = utils.getValidatedStages(chosenStages, pipelineStages)
+
     stages.each{
-        stage('test'){
-            utils.isCIorCD()
+        stage(it){
+            try {
+                "${it}"()
+            }
+            catch(Exception e) {
+                error "Stage ${it} tiene problemas: ${e}"
+            }
         }
     }
 }
+
+def crearRelease(){
+    if (env.GIT_BRANCH.contains('develop')){
+        
+        def git = new git.GitMethods()
+
+        if (git.checkIfBranchExists('release-v1-0-0')){
+            println 'La rama existe'
+            git.deleteBranch('release-v1-0-0')
+            println 'Rama eliminada. Se crea nuevamente.'
+            git.createBranch(env.GIT_BRANCH,'release-v1-0-0')
+            println 'Rama creada con éxito.'
+        } else {
+            git.createBranch(env.GIT_BRANCH,'release-v1-0-0')
+            println 'Rama creada con éxito.'
+        }
+
+    } else {
+        println "La rama ${env.GIT_BRANCH} no corresponde como rama de orígen para la creación de un Release."
+    }
+}
+
+def compile(){
+    sh './mvnw clean compile -e'
+}
+
+def test(){
+    sh './mvnw clean test -e'
+}
+
+def jar(){
+    sh './mvnw clean package -e'
+}
+
+def sonar(){
+    withSonarQubeEnv(installationName: 'sonar-server') {
+        sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+    }
+}
+
+def runJar(){
+    sh 'nohup bash mvnw spring-boot:run &'
+    sleep 20
+}
+
+def rest(){
+    sh "curl -X GET http://localhost:8082/rest/mscovid/test?msg=testing"
+}
+
+def nexusCI(){
+    nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: "build/DevOpsUsach2020-0.0.1.jar"]], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: "0.0.1-${env.GIT_BRANCH}"]]]  
+}
+
+def downloadNexus(){
+    sh "curl -X GET -u admin:admin http://localhost:8081/repository/test-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1-develop/DevOpsUsach2020-0.0.1-develop.jar -O"
+}
+
+def runDownloadedJar(){
+    sh "nohup java -jar DevOpsUsach2020-0.0.1-develop.jar &"
+    sleep 20
+}
+
+def nexusCD(){
+    nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: "DevOpsUsach2020-0.0.1-develop.jar"]], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]  
+}
+
+return this;
