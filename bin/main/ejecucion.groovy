c5e13cc1c733f6d25bd099c567221ca269cecf16
Diego Inostroza <diego.inoca@gmail.com>
2022-02-06 17:14:33 -0300

    avances

--- bin/main/ejecucion.groovy
+++ bin/main/ejecucion.groovy
@@ -3,7 +3,6 @@ def call(){
     pipeline {
         agent any
         parameters { 
-            choice(name: 'buildtool', choices: ['maven','gradle'], description: 'Elección de herramienta de construcción para aplicación covid')
             string(name: 'stages', defaultValue: '' , description: 'Escribir stages a ejecutar en formato: stage1;stage2;stage3. Si stage es vacío, se ejecutarán todos los stages.')
         }
 
@@ -12,11 +11,11 @@ def call(){
                 steps {
                     script{
                         sh 'env'
-                        figlet params.buildtool
-                        def archivo = (params.buildtool == 'gradle') ? 'build.gradle' : 'pom.xml'
-                        if (fileExists(archivo)){
-                            "${params.buildtool}" "${params.stages}"
-                        } else {
+                        def buildtool = 'maven'
+                        if (fileExists('pom.xml')){
+                            figlet params.buildtool
+                            "${buildtool}" "${params.stages}"
+                        }   else{
                             error "archivo ${archivo} no existe. No se puede construir pipeline basado en ${params.buildtool}"
                         }
                     }
